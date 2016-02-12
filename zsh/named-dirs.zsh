4fb6fcb9f6cd052790a256b5724f765065f440b8
Samuel Overington (neon) <so@madeso.uk>
2016-02-12 22:08:11 +0000

    ZSH added Named direcoties and ssh aliases

---
+++ zsh/named-dirs.zsh
@@ -0,0 +1,4 @@
+Work=/Users/samuel/Work
+Sites=/Users/samuel/Sites
+
+Stll=/Users/samuel/Work/Stillnessinyoga
---
+++ zsh/ssh-aliases.zsh
@@ -0,0 +1,5 @@
+# Sites
+#
+# madeso.uk
+
+alias Madeso-theme="ssh -t madeso 'cd ~/public_html/wp-content/themes/made/ ; bash'"
